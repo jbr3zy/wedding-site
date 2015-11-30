47caaf831c032deb5b50ec8fa5f9a68e063cbd6b
Justin Butler <butler.jus@gmail.com>
2015-11-29 16:01:51 -0800

    lots of responsive styling

--- client/js/data.js
+++ client/js/data.js
@@ -1,14 +1,14 @@
 module.exports = [
     {
-        src: "https://drive.google.com/uc?id=0B0HS5TH7JBQLV2g0RXBsaFdXYVU",
-        msrc: "https://drive.google.com/uc?id=0B0HS5TH7JBQLV2g0RXBsaFdXYVU",
-        w: 1500,
-        h: 820
-    },
-    {
         src: "https://drive.google.com/uc?id=0B0HS5TH7JBQLMDE0YkFMeGw2VUU",
         msrc: "https://drive.google.com/uc?id=0B0HS5TH7JBQLMDE0YkFMeGw2VUU",
         w: 820,
+        h: 820
+    },
+    {
+        src: "https://drive.google.com/uc?id=0B0HS5TH7JBQLV2g0RXBsaFdXYVU",
+        msrc: "https://drive.google.com/uc?id=0B0HS5TH7JBQLV2g0RXBsaFdXYVU",
+        w: 1500,
         h: 820
     },
     {
